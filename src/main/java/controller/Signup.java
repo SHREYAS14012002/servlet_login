1af1ec13fd68cb4d604b7c48eb7cc8f77ff99c08
SHREYAS14012002 <139356449+SHREYAS14012002@users.noreply.github.com>
2023-11-04 20:48:41 +0530

    Update Signup.java

--- src/main/java/controller/Signup.java
+++ src/main/java/controller/Signup.java
@@ -16,14 +16,14 @@ public class Signup extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		User u = new User();
+		
 
 		String name = req.getParameter("name");
 		String email = req.getParameter("email");
 		long phone = Long.parseLong(req.getParameter("phone"));
 		String password = req.getParameter("password");
 		
-		
+		User u = new User();
 		u.setName(name);
 		u.setPhno(phone);
 		u.setEmail(email);
